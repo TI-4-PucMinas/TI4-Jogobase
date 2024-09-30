19a78f6fa145e270aacdcfc580d24994de06553d
Pedro Augusto <127454807+DE4THB0RN@users.noreply.github.com>
2024-09-30 12:41:29 -0300

    AAAAAAAAAAAAAAAHHHHH
    A DOR
    ELA NÃO PARA
    FAZ PARAR

--- Assets/Scripts/Fighters/Protagonista/Protagonista - Cópia .cs
+++ Assets/Scripts/Fighters/Protagonista/Protagonista - Cópia .cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Protagonista_Copy : Player_Copy
+public class Protagonista : Player
 {
     
     void Start()
