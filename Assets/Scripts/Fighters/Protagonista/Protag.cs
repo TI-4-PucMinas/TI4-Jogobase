9025cd106472000c1ed82905094fdb0c5bf3f954
Pedro Augusto <127454807+DE4THB0RN@users.noreply.github.com>
2024-09-30 13:06:51 -0300

    AAAAAAAGGGGG

--- Assets/Scripts/Fighters/Protagonista/Protag.cs
+++ Assets/Scripts/Fighters/Protagonista/Protag.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Protagonista : Player
+public class Protag : Player
 {
 
     protected PlayerControls controls;
