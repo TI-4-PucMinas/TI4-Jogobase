b3714d97919b9b268840c6d9f3cc95c5ca5af9b7
Rafael Vilela Padilha Clark <rafavpadilhaclark@gmail.com>
2024-10-03 21:46:17 -0300

    Versão mais atualizada
    Versão com o controle da câmera e com a tela de vitoria colocada novamente

--- Assets/Scripts/FigthManager.cs
+++ Assets/Scripts/FigthManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManeger : MonoBehaviour
 {
@@ -13,11 +14,23 @@ public class SceneManeger : MonoBehaviour
     private GerenciadorDvida vida1;
     private GerenciadorDvida vida2;
 
+
+    // Limites da tela
+    private float minX, maxX;
+
     // Start is called before the first frame update
     void Start()
     {
         vida1 = player.GetComponent<GerenciadorDvida>();
         vida2 = enemy.GetComponent<GerenciadorDvida>();
+        // Definir o tamanho ortográfico fixo da câmera
+        Camera.main.orthographicSize = 5.265842f;
+
+        // Ajustar o scale da câmera ao mínimo possível, mantendo a proporção da tela
+        AdjustCameraScale();
+
+        // Calcular os limites da câmera
+        UpdateCameraLimits();
     }
 
     // Update is called once per frame
@@ -35,6 +48,7 @@ public class SceneManeger : MonoBehaviour
         if(vida2.VidaAtual <= 0 && gaming)
         {
             Debug.Log("Player 1 ganhou");
+            SceneManager.LoadScene("Vitoria");
             gaming = false;
         }
         else if(vida1.VidaAtual <= 0 && gaming)
@@ -42,5 +56,23 @@ public class SceneManeger : MonoBehaviour
             Debug.Log("Player 2 ganhou");
             gaming = false;
         }
+    }
+
+    void UpdateCameraLimits()
+    {
+        // Calcular os limites da câmera em coordenadas do mundo
+        Vector3 bottomLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
+        Vector3 topRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.nearClipPlane));
+
+        // Definir os limites com base nas bordas da câmera
+        minX = bottomLeft.x;
+        maxX = topRight.x;
+    }
+
+    // Ajusta a escala da câmera mantendo a proporção da tela
+    void AdjustCameraScale()
+    {
+        float aspectRatio = (float)Screen.width / (float)Screen.height;
+        Camera.main.aspect = aspectRatio;
     }
 }
